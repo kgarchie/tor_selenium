9dfe7104928232d34ee1aab65d4b5ff19f161b5f
Bosire Allan <archiethebig@gmail.com>
2020-07-31 01:51:43 +0300

    Add files via upload

---
+++ venv_test.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import os
+import time
+from stem.control import Controller
+from stem import Signal
+from stem import process
+
+# To use Tor's SOCKS proxy server with chrome, include the socks protocol in the scheme with the --proxy-server option
+# PROXY = "socks5://127.0.0.1:9150" # IP:PORT or HOST:PORT
+
+
+tor_launcher = process.launch_tor_with_config(
+  config = {
+    'ControlPort': '9051',
+    },
+)
+# torexe = os.popen(r'C:\Users\Kuski\Desktop\Tor Browser\Browser\TorBrowser\Tor\tor.exe')
+PROXY = "socks5://127.0.0.1:9050" # IP:PORT or HOST:PORT
+options = webdriver.ChromeOptions()
+options.add_argument('--proxy-server=%s' % PROXY)
+driver = webdriver.Chrome(options=options, executable_path=r'C:\Users\Kuski\Desktop\chromedriver.exe')
+driver.get("http://www.icanhazip.com")
+time.sleep(5)
+driver.get("http://check.torproject.org")
+time.sleep(5)
+
+
+with Controller.from_port(port=9051) as controller:
+    controller.authenticate()
+    print("successful authenticate")
+    controller.signal(Signal.NEWNYM)
+    
+driver.get("http://www.icanhazip.com")
+time.sleep(5)
+tor_launcher.terminate()
+driver.quit()   
+
